777fa11f3d3ba5e4be873e1eca3575862ca38c9d
Alien Gurung <gurungalien2727@gmail.com>
2021-11-19 20:45:40 -0600

    removed unused imports

--- components/Users.js
+++ components/Users.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { FlatList} from 'react-native';
 import useFetchUsers from './hooks/useFetchUsers';
 import Loading from './Loading';
